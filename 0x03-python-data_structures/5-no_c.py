1c2b5ed475ad52e24e843edef44f4061627caca4
cristian <cristaker1927@hotmail.com>
2022-01-03 15:25:44 -0800

    point 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    newstr = ""
+    for i in range(len(my_string)):
+        if my_string[i] == "C" or my_string[i] == "c":
+            pass
+        else:
+            newstr = newstr + my_string[i]
+    return (newstr)
