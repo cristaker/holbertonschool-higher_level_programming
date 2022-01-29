329e472b56bea0d89fb608ce2c1eb6312e7be5af
cristian <cristaker1927@hotmail.com>
2022-01-29 15:07:54 -0800

    point 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+"""print #"""
+
+
+def print_square(size):
+    """print #"""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print('#' * size)
