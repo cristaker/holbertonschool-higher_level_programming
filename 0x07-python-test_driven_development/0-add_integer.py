ade5946588dad013b0e070862576274ed60e52b2
cristian <cristaker1927@hotmail.com>
2022-01-28 17:43:02 -0800

    point 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+
+"""Test Driven Development"""
+
+
 def add_integer(a, b=98):
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
