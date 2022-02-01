def7c645299d9ee2d20b90d8c4bbb01f822d6deb
cristian <cristaker1927@hotmail.com>
2022-01-31 20:11:54 -0800

    point 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Method down"""
+
+    def __init__(self, size):
+        """instantiation initialized"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """area instance"""
+
+        return self.__size * self.__size
+
+    def __str__(self):
+        """self instance"""
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
