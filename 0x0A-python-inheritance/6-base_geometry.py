a985b7de6f99d79a1b770c718d2637f0dec207fc
cristian <cristaker1927@hotmail.com>
2022-01-31 18:07:28 -0800

    point 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""Class base geometry"""
+
+
+class BaseGeometry:
+    """method down"""
+
+    def area(self):
+        """instance"""
+        raise Exception("area() is not implemented")
