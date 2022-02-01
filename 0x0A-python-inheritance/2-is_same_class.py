9fd0c1b021773fbe35541cb19e20400b99260b09
cristian <cristaker1927@hotmail.com>
2022-01-31 17:33:59 -0800

    point 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""Function prove if the object is an instance"""
+
+
+def is_same_class(obj, a_class):
+    """Method"""
+
+    if obj == isinstance(obj, a_class):
+        return True
+    else:
+        return False
