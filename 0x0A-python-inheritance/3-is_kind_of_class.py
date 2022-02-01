d36d7373812a8799c5879f30378791534aae4e8b
cristian <cristaker1927@hotmail.com>
2022-01-31 17:46:52 -0800

    point 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""function that returns True if the object is an instance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """method that prove it"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
