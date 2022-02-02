8c2043cd0d68ca41c60f358756675f55a3d3d21c
cristian <cristaker1927@hotmail.com>
2022-02-01 18:36:56 -0800

    point 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Class define a student"""
+
+
+class Student:
+    """Method down"""
+
+    def __init__(self, first_name, last_name, age):
+        """instantiation"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+
+        return self.__dict__
