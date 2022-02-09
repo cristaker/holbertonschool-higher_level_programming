29ebb6d4a6005a0354d30dc8abfaca3605a1cd6c
cristian <cristaker1927@hotmail.com>
2022-02-08 17:33:37 -0800

    point 22

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,9 +8,29 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """instance"""
-        
+
         super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
