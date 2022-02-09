e8ad35a0c6c2b5ccef48f7d237c6574090b26687
cristian <cristaker1927@hotmail.com>
2022-02-09 15:55:25 -0800

    point 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,3 +31,21 @@ class Square(Rectangle):
         """class update"""
         return "[Square] ({}) {}/{} - {}".format(
                 self.id, self.x, self.y, self.height)
+
+    def update(self, *args, **kwargs):
+        """update"""
+        if args is not None and len(args) is not 0:
+            list_atr = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if list_atr[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, list_atr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
