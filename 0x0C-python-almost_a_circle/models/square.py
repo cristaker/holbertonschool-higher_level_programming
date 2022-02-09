f58f76c4c190b406f2bf0f639754044fec8327b2
cristian <cristaker1927@hotmail.com>
2022-02-09 15:25:33 -0800

    point 9

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """special method"""
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
