012d9064a39852f8aa65da64bf2cf249ed18740a
cristian <cristaker1927@hotmail.com>
2022-03-15 06:10:54 -0700

    point 8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- Display number of records
 -- The database name will be passed as an argument
-SELECT COUNT(*) AS id FROM first_table where id=89;
+SELECT COUNT(*) as id FROM first_table WHERE id=89;
