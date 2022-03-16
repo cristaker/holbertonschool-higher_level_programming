a830d57fcc4b78b52db09a0baf907595341f518b
cristian <cristaker1927@hotmail.com>
2022-03-16 13:41:34 -0700

    point 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- script that creates the MySQL server user user_0d_1
+-- all privileges on your MySQL server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
