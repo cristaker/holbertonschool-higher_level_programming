7cf608ed1b23f413acb3639802e69c3cc8c40b2c
cristian <cristaker1927@hotmail.com>
2022-04-07 16:32:57 -0700

    point 100

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -10,10 +10,10 @@ class Rectangle {
   print () {
     for (let i = 0; i < this.height; i++) {
       let s = '';
-        for (let j = 0; j < this.width; j++) {
-          s += 'X';
-	}
-    console.log(s);
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
     }
   }
 
