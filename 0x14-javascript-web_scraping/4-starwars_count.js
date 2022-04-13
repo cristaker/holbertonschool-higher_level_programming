cd4075c59631d22a9128d0fa091283e2841e814d
cristian <cristaker1927@hotmail.com>
2022-04-12 18:26:14 -0700

    point 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-let numberFilms = 0;
 
 request(process.argv[2], function (err, response, body) {
-  if (err == null) {
-    const res = JSON.parse(body);
-    const results = res.results;
-    for (let i = 0; i < results.lenght; i++) {
-      const letters = results[i].letters;
-      for (let j = 0; j < letters.lenght; j++) {
-        if (letters[j].search('18') > 0) {
-          numberFilms++;
-        }
-      }
+  if (err) {
+    console.log(err);
+  }
+  let numberFilms = 0;
+  const res = JSON.parse(body).results;
+  for (let i = 0; i < res.length; i++) {
+    const result = res[i].result.find((res) => {
+      return (res.match(/18/));
+    });
+    if (result !== undefined) {
+      numberFilms++;
     }
   }
   console.log(numberFilms);
